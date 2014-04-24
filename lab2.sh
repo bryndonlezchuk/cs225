e3c0e68a963e925a7457234601451fdb80a9943a
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2014-04-24 15:08:20 -0700

    lab2 initial commit lab1 optimizing

---
+++ lab2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /root/medialab
+
+MEDIA=$(grep filename media.xml | grep -v '><' | grep -v 'gportal' | grep -v 'NULL' | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }')
+
+COUNT=0
+
+for ITEM in $MEDIA
+do
+	if [ ! -f "$ITEM" ]
+	then
+		echo "$ITEM"
+		(( COUNT++ ))
+	fi
+done
+echo "$COUNT items not found"
+
