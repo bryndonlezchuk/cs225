32e632e45084cccb42b996ee64d73879c98d1305
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2014-04-24 17:32:34 -0700

    initial commit for extra credit

---
+++ lab2x.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /root/bin
+
+./lab1.sh
+./lab2.sh
--- lab2xb.sh
+++ lab2xb.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-cd /root/medialab
+cd /root/medialab/
+
+LIST=$(ls | grep -v media.xml)
+
+
+COUNT=0
+
+for ITEM in $LIST
+do
+
+	if ! grep -q "$ITEM" media.xml
+	then
+		echo "$ITEM not found in media.xml"
+		(( COUNT++ ))
+	fi
+done
+echo "$COUNT items not found"
+
 
 MEDIA=$(grep -e ".mpg" -e ".mp3" media.xml | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' | sort -u)
 
