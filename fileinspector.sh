dfdcb7a2f8752e4d263870788fd61c6251046a76
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2014-05-08 17:33:18 -0700

    fileinspector.sh (lab 6) -- ready for submit 	Still looking to add: 		~Check to see if filetable.txt is in designated directory 		~check to see if the extention is missing from filetable.txt

--- fileinspector.sh
+++ fileinspector.sh
@@ -1,45 +1,69 @@
 #!/bin/bash
 #set -x
 
-ARG=$1
+ARG="$1"
 FILETABLE="/root/bin/filetable.txt"
 
-FILE=${ARG##*/}
-DIR=${ARG%/*}
+FILE="${ARG##*/}"
+DIR="${ARG%/*}"
 
-if ! cd $DIR
+if [[ -z "$DIR" ]]
+then
+	echo "Do you want to run fileinspector on the current directory? (default no)"
+	read INPUT
+
+	if [[ "$INPUT" =~ [yY] || "$INPUT" =~ [yY][eE][sS] ]]
+	then
+		DIR="."
+	else
+		exit
+	fi
+fi
+
+if ! cd "$DIR"
 then
 	echo "Invalid directory"
 	exit 1
 fi
 
-if [[ -z $FILE ]]
+if [[ -z "$FILE" ]]
 then
 	#Directory
-	SET=$(ls)
+	SET="$(ls)"
 else
 	#File
 
 	#Check if file exist
-	if ! ls $FILE &> /dev/null
+	if ! ls "$FILE" &> /dev/null
 	then
 		echo "File does not exist"
 		exit 1
 	fi
 
-	SET=$FILE
+	SET="$FILE"
 fi
 
 #for each item, match to mime
 #from the mime type, apply file extention
 for ITEM in $SET
 do
-	echo "filetable.txt contains:"
-	echo $FILETABLE
-
-	MIME=$(file --mime-type $ITEM | awk '{print $2}')
-	echo "Searching for type $MIME"
+	MIME="$(file --mime-type $ITEM | awk '{print $2}')"
+	echo "Searching for type $MIME for file $ITEM"
 	
-	FILETYPE=$( grep $MIME $FILETABLE)
+	FILETYPE="$( grep $MIME $FILETABLE)"
 	echo "Found $FILETYPE"
+
+	FILETYPE="${FILETYPE##*#@#}"
+	echo "Filetype for $ITEM is $FILETYPE"
+
+	if [[ -z "${ITEM##*$FILETYPE}" ]]
+	then
+		echo "Extention already exists"
+		echo
+		continue
+	else
+		echo "Applying extention $FILETYPE to $ITEM"
+		echo
+		mv "$ITEM" "${ITEM}.$FILETYPE"
+	fi
 done
