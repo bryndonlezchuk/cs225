9590a07562ba942655575ca44bbfc9e66fed2f20
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2014-05-08 20:36:12 -0700

    fileinspector.sh (lab 6) -- adding content 	! Line 81 - empty if/else statement

--- fileinspector.sh
+++ fileinspector.sh
@@ -7,6 +7,22 @@ FILETABLE="/root/bin/filetable.txt"
 FILE="${ARG##*/}"
 DIR="${ARG%/*}"
 
+while [[ ! -f "$FILETABLE" ]]
+do
+	echo "Filetable not found. Would you like to specify a file? (default yes)"
+	read INPUT
+
+	if [[ "$INPUT" =~ [nN] || "$INPUT" =~ [nN][oO] ]]
+	then
+		echo "Exiting fileinspector"
+		exit
+	else
+		echo "Please enter directory for the filetable:"
+		read FILETABLE
+		echo
+	fi
+done
+
 if [[ -z "$DIR" ]]
 then
 	echo "Do you want to run fileinspector on the current directory? (default no)"
@@ -34,7 +50,7 @@ else
 	#File
 
 	#Check if file exist
-	if ! ls "$FILE" &> /dev/null
+	if [[ ! -f "$FILE" ]]
 	then
 		echo "File does not exist"
 		exit 1
@@ -50,11 +66,24 @@ do
 	MIME="$(file --mime-type $ITEM | awk '{print $2}')"
 	echo "Searching for type $MIME for file $ITEM"
 	
-	FILETYPE="$( grep $MIME $FILETABLE)"
-	echo "Found $FILETYPE"
+	FILETYPE="$(grep $MIME $FILETABLE)"
+	
+	if [[ ! -z "$FILETYPE" ]]
+	then
+		echo "Found $FILETYPE"
 
-	FILETYPE="${FILETYPE##*#@#}"
-	echo "Filetype for $ITEM is $FILETYPE"
+		FILETYPE="${FILETYPE##*#@#}"
+		echo "Filetype for $ITEM is $FILETYPE"
+	else
+		echo "Filetype not found in filetable. Would you like to add it? (default no)
+		read INPUT
+
+		if [[ "$INPUT" =~ [yY] || "$INPUT" =~ [yY][eE][sS] ]]
+		then
+			
+		else
+		fi
+	fi
 
 	if [[ -z "${ITEM##*$FILETYPE}" ]]
 	then
